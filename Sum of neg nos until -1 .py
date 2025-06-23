611e041c9ac611bf9e5e429ab3ef63ed67cdd9c0
Reshmadhayalan <reshmadhayalan29@gmail.com>
2025-06-23 08:50:14 +0530

    Create Sum of neg nos until -1 .py

---
+++ Sum of neg nos until -1 .py
@@ -0,0 +1,8 @@
+total = 0
+while True:
+    num = int(input("Enter a number (-1 to stop): "))
+    if num == -1:
+        break
+    if num < 0:
+        total += num
+print("Sum of negative numbers:", total)
